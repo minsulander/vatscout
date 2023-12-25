98c3b6a5882b7072cab5988e0ca1cc763b13cb05
Martin Insulander <martin@insulander.com>
2023-12-25 16:22:29 +0100

    Layout update

--- src/layouts/default/View.vue
+++ src/layouts/default/View.vue
@@ -1,15 +1,15 @@
 <template>
     <v-main>
         <router-view />
-        <hr color="#222" class="mt-5"/>
-        <v-row class="text-caption text-grey-darken-3 px-1">
-            <v-col cols="6"> Copyright &copy; 2023 VATScout project contributors </v-col>
-            <v-col cols="6" class="text-right">
-                <a class="text-grey-darken-3" href="https://github.com/minsulander/vatscout"
-                    ><v-icon>mdi-github</v-icon> VATScout on GitHub</a
-                >
-            </v-col>
-        </v-row>
+            <hr color="#222" class="mt-5" />
+            <v-row class="text-caption font-weight-light text-grey-darken-3 mx-1 mb-1">
+                <v-col cols="6">&copy; 2023 VATScout project contributors </v-col>
+                <v-col cols="6" class="text-right">
+                    <a class="text-grey-darken-3" href="https://github.com/minsulander/vatscout"
+                        ><v-icon>mdi-github</v-icon> VATScout on GitHub</a
+                    >
+                </v-col>
+            </v-row>
     </v-main>
 </template>
 
