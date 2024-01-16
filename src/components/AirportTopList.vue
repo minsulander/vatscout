d111b0177096cad2beea7886f08debaff85a1b69
Martin Insulander <martin@insulander.com>
2024-01-16 09:42:17 +0100

    Add empty text to airport list

--- src/components/AirportTopList.vue
+++ src/components/AirportTopList.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <div v-if="activeAirports.length == 0" class="mt-2 text-caption text-grey-darken-2 font-weight-light text-center">
+            - NO DEPARTURES, ARRIVALS OR CONTROLLERS ACTIVE -
+        </div>
         <v-row v-for="airport in activeAirports" :key="airport.icao" no-gutters class="pa-1" @click="clickAirport(airport)">
             <v-col cols="6" sm="1">
                 {{ airport.icao }}
